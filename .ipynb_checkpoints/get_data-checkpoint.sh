42c194d19a8d37c51590ea1e641fbbda22378997
Joel Joseph <34275997+joeljosephjin@users.noreply.github.com>
2021-02-16 11:46:33 +0530

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/get_data-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-pip uninstall -y enum34
-pip install gdown
-gdown https://drive.google.com/u/0/uc?id=1rV3aj_hgfNTfCakffpPm7Vhpr1in87CR
-mkdir data
-tar -xvzf miniImagenet.tgz -C data/
-rm miniImagenet.tgz
